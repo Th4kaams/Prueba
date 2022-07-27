21b23788fe416cbd8920c3fc7613728f33d2a292
Habib Gutierrez <th4.kaams@gmail.com>
2022-07-26 22:33:09 -0500

    Hola Mundo
    Agregamos un script de Hola mundo

---
+++ Proyecto EBAC/Assets/Scripts/Holamundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holamundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola no se que pasa");
+        Debug.LogError("Paso lago feo");
+        Debug.LogWarning("Algo pasa aquí");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
